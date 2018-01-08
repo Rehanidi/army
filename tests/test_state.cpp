0d890b5f7a5767052bb261609a36403bdf259384
Rehanidi <rehanidi@gmail.com>
2018-01-08 16:34:50 +0200

    Create folder with the tests

---
+++ tests/test_state.cpp
@@ -0,0 +1,22 @@
+#include "catch.hpp"
+#include "../state/State.h"
+
+TEST_CASE("Test state class"){
+	State* state = new State("State", 200, 50);
+
+	REQUIRE(state->getTitile() == "State");
+	REQUIRE(state->getHitPoints() == 200);
+	REQUIRE(state->getHitPointsLimit() == 200);
+	REQUIRE(state->getDamage() == 50);
+
+	SECTION("State::takeDamage tests"){
+		int damage = 10;
+		int iteration = state->getHitPoints() / damage;
+
+		for( int i = 1 ; i <= iteration; i++ ){
+			state->takeDamage(damage);
+			
+		}
+	}
+
+}
